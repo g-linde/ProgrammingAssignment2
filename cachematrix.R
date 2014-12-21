01e2ccbb35f05eb63248d15143cde9fb9ab2b5d0
g-linde <greg@thelindefamily.net>
2014-12-21 07:48:47 -0500

    First draft
    Just adding minimal comments to structure

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion caching - programming assignment #2 in R Programming course
+## Greg Linde - December 2014
+## Create functions to return the inverse of a matrix, and cache the result to
+## avoid unnecessary computations when possible
 
-## Write a short comment describing this function
+## makeCacheMatrix - 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    getInv <- functionx(solve) m <<- solve
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
 }
